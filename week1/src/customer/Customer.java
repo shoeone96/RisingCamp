0c7da79508f3f7223a3d60b5634b773d72ebbdf4
shoeone96 <dlwnddnjs96@gmail.com>
2023-07-12 03:25:38 +0900

    Customer 추상 클래스 선언

---
+++ week1/src/customer/Customer.java
@@ -0,0 +1,47 @@
+package customer;
+
+import benefit.BenefitMethod;
+
+public abstract class Customer {
+    private String name;
+    private Grade grade;
+    private BenefitMethod benefitMethod;
+    private int bonus;
+
+    public Customer(String name, String grade, BenefitMethod method) {
+        this.name = name;
+        this.grade = Grade.setGrade(grade);
+        this.benefitMethod = method;
+        this.bonus = 0;
+    }
+
+    public abstract void showCustomerInfo();
+
+    public void calculatePrice(int price) {
+        this.benefitMethod.form(price, grade.getRate());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Grade getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Grade grade) {
+        this.grade = grade;
+    }
+
+    public int getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(int bonus) {
+        this.bonus = bonus;
+    }
+}
